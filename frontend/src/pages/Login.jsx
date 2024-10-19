30a5f03720c2421c46c2aadfa876ae3b7f47102d
muli-shem <mulishem2002@gmail.com>
2024-10-19 13:50:24 +0300

    feat:Login pages

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,44 @@
+import React  from "react";
+import { useForm } from "react-hook-form";
+import './Login.scss';
+import axios from "axios";
+
+const Login = () => {
+    const { register, handleSubmit, formState:{errors}} = useForm();
+
+
+const onSubmit = async (data) => {
+    try {
+        const response = await axios.post('http://localhost:8080/api/login', {
+            email: data.email,
+            password: data.password,
+        });
+        console.log('You have logged in successfully');
+    } catch (error) {
+        console.error('Login failed',error);
+    }
+}
+
+return(
+    <div className="login-container">
+        <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+            <h1>Login</h1>
+            <input
+             type="email" 
+             placeholder="Email" 
+             {...register('email', {required: true, pattern: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/})} />
+            {errors.email && <span className="error-message">Please enter a valid email address</span>}
+            <input
+             type="password" 
+             placeholder="Password" 
+             {...register('password', {required: true, minLength: 8})} />
+             {errors.password && <span className="error-message">Password must be at least 8 characters long</span>}
+             <button type="submit">Login</button>
+             <p>Don't have an account? <a href="#">Sign up</a></p>  
+             <p>Forgot your password? <a href="#">Reset password</a></p> 
+        </form>
+            
+    </div>
+)
+};
+export default Login;
