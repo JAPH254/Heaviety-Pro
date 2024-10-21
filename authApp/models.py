4f4dfd5391b61fe53149ea43f88ee83eb29c8752
JAPH254 <omanyujapheth@gmail.com>
2024-10-21 09:34:26 +0300

    feat: add username and password as the requirred fields

--- authApp/models.py
+++ authApp/models.py
@@ -20,6 +20,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
 
+    REQUIRED_FIELDS = ['first_name', 'last_name']
     @property
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
