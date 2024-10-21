5821b9d4e6fdf1ae5990eacd56ad58b4dc82cf9e
Joyce Wambui <joycewambui@gmail.com>
2024-10-21 15:11:53 +0300

    deleted some files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,6 @@
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
 import Home from './pages/Home'; 
-import InputField from './features/register/inputfield.jsx';
-import FormHeader from './features/register/formHeader.jsx';
-import FormButton from  './features/register/formButton.jsx'
 import Register from './features/register/register.jsx';
 
 function App() {
