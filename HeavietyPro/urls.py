79f78d7e373cce826111d2a225fec77a60179e05
JAPH254 <omanyujapheth@gmail.com>
2024-10-26 07:20:05 +0300

    feat: added the djoser url routing

--- HeavietyPro/urls.py
+++ HeavietyPro/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+admin.site.site_title = "HEAVIETY ADMIN"
+admin.site.site_header = "HEAVIETY ADMINISTRATION"
+admin.site.index_title = "Welcome to Heaviety administration"
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("auth/", include('djoser.urls')),
+    path("auth/", include('djoser.urls.jwt'))
 ]
