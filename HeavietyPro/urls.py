b4734bc83f41b89df45a6df4216281b1ee0bfcf0
JAPH254 <omanyujapheth@gmail.com>
2024-10-26 08:51:14 +0300

    fix: remove the unused imports

--- HeavietyPro/urls.py
+++ HeavietyPro/urls.py
@@ -15,10 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include,re_path
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.urls import path,include
 
 admin.site.site_title = "HEAVIETY ADMIN"
 admin.site.site_header = "HEAVIETY ADMINISTRATION"
