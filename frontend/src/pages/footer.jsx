fb7f26c12b9086d39abd5b990366b2206e1975b9
Joyce Wambui <joycewambui@gmail.com>
2024-11-15 11:04:53 +0300

    feat:adding the new pages directory

---
+++ frontend/src/pages/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-blue-800 text-white text-center p-4 mt-auto flex-grow">
+      <p>&copy; 2024 Heaviety. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
