aa92bc7e91be18fe61cf980eefeed7cf1ae2dfe9
Joyce Wambui <joycewambui@gmail.com>
2024-11-19 16:15:30 +0300

    feat:making some changes

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,15 @@
+import Navbar from './navbar';
+import LandingPage from './landingPage';
+import Footer from './footer';
+
+function Home() {
+  return (
+    <> 
+      <Navbar />
+      <LandingPage />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
