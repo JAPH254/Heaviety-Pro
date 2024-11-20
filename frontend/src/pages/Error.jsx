bf24ec0dac86b78e8179237cc13c5b52b7260a45
muli-shem <mulishem2002@gmail.com>
2024-11-20 05:35:17 +0300

    fix: fixed deepSource errors

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
 import { Link } from "react-router-dom";
-import {ArrowLeft} from "lucide-react"
-import "./Error.scss"
+import { ArrowLeft } from "lucide-react";
+import "./Error.scss";
 
-function Error({statusText, message}) {
+function Error({ statusText, message }) {
   return (
     <div className="error-container">
-        <div className="error-content">
-            <p className="error-code">404</p>
-            <h1 className="error-message">Sorry, we couldn't find the page you're looking for.</h1>
-            {statusText||message && <p className="error-detail">{statusText || message}</p>}
-            <div className="error-actions">
-                <Link to="/" className="error-action">
-                <ArrowLeft size={24}  className="error-icon"/>
-                GO BACK
-                </Link>
-            </div>
-
+      <div className="error-content">
+        <p className="error-code">404</p>
+        {/* Escaped single quote */}
+        <h1 className="error-message">
+          Sorry, we couldn&apos;t find the page you&apos;re looking for.
+        </h1>
+        {(statusText || message) && (
+          <p className="error-detail">{statusText || message}</p>
+        )}
+        <div className="error-actions">
+          <Link to="/" className="error-action">
+            <ArrowLeft size={24} className="error-icon" />
+            Go Back
+          </Link>
         </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
