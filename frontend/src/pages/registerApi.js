ba06a383ce44f6be35ac7844b13de13b8f05f554
Joyce Wambui <joycewambui@gmail.com>
2024-11-21 01:23:36 +0300

    feat:added some files

--- frontend/src/pages/registerApi.js
+++ frontend/src/pages/registerApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const registerApi = createApi({
   reducerPath: "registerApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "",
+    baseUrl: "http://127.0.0.1:8000/auth/users/",
   }),
   endpoints: (builder) => ({
     // Login user mutation
