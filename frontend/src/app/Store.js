0b84c83fa8da51733df4e29389ffefedc04119c8
muli-shem <mulishem2002@gmail.com>
2024-12-03 10:56:50 +0300

    feat: added registerAPI

--- frontend/src/app/Store.js
+++ frontend/src/app/Store.js
@@ -2,12 +2,9 @@ import {configureStore, combineReducers} from "@reduxjs/toolkit";
 import {persistReducer, persistStore} from "redux-persist";
 import storage from 'redux-persist/lib/storage';
 import {setupListeners} from "@reduxjs/toolkit/query";
+import authReducer from "../pages/Login/loginSlice"
+import registerApi from "../pages/Register/registerApi";
 
-import authReducer from "../pages/loginSlice"
-
-// import userProfileReducer from '../pages/userProfileSlice'; 
-// import registerApi from "../pages/registerApi";
-import registerApi from "../pages/registerApi";
 
 
 const persistConfig ={
@@ -17,9 +14,6 @@ const persistConfig ={
 };
 const rootReducer = combineReducers({
     auth:authReducer,
-
-    // auth:userProfileReducer,
-    // [registerApi.reducerPath]:registerApi.reducer  
     [registerApi.reducerPath]:registerApi.reducer
 });
 const persistedReducer  = persistReducer(persistConfig,rootReducer);
