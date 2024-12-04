716a6d5a0f7a934c69e4b43812a0464f0c94b90d
JAPH254 <omanyujapheth@gmail.com>
2024-12-04 19:02:56 +0300

    ffix: removed unused imports

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,14 +1,11 @@
 
-import Navbar from './navbar';
 import LandingPage from './landingPage';
-import Footer from './footer';
+
 
 function Home() {
   return (
-    <> 
-      <Navbar />
+    <>
       <LandingPage />
-      <Footer />
     </>
   );
 }
